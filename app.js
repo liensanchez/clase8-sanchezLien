692c06c0163bc3b64a7790fbed6442eaba986fc0
Lien! <liensilviosanchez@gmail.com>
2022-01-16 21:58:28 -0300

    CLASE8

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 
-//CREANDO UN ARRAY DE CLASES ANTERIORES CON OBJETO
+//CREANDO UN OBJETO DE CLASES ANTERIORES CON OBJETO
 class pilas{
   constructor(tipo,precio){
     this.tipo=tipo;
     this.precio=parseFloat(precio);
   }
 }
+
+//ARRAY DE BATERIAS
 let arrayBaterias=[];
 
 arrayBaterias.push(new pilas("aaa","200"));
 arrayBaterias.push(new pilas("aa","200"));
+//PIDIENDO DATOS CON PROMPT
 arrayBaterias.push(new pilas(prompt("tipo"),prompt("precio")));
 
 
 
-//lista de baterias con DOM
+//LISTA DE BATERIAS CON DOM
 let tituloBaterias = document.createElement("ul");
 tituloBaterias.innerHTML="<ul>baterias</ul>";
 document.body.appendChild(tituloBaterias);
 
-//agregar baterias con DOM
+//AGREGAR CON DOM A LA LISTA
 let listaBaterias = document.getElementById("arrayBaterias");
 
 for (const pila of arrayBaterias){
