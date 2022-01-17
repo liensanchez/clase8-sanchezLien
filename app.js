7160cffca94b24b99f0aa7a9dc0c4deabe7f4a10
Lien! <liensilviosanchez@gmail.com>
2022-01-16 21:56:57 -0300

    clase 8

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-class Pilas{
+
+//CREANDO UN ARRAY DE CLASES ANTERIORES CON OBJETO
+class pilas{
   constructor(tipo,precio){
-  this.tipo=tipo,
-  this.precio=precio;
-  this.cantidad=10;
-  this.stock=false;
-  this.fallados=0;
-  }
-
-  //SUMAR IVA
-  Iva (){
-  this.precio=this.precio*1.21;
-    }
-
-  //CAMBIAR EL STOCK DE TRUE A FALSE
-  Venta(){
-    this.stock=true;
-  }
-
-  //QUITAR PRODUCTOS POR VENTAS
-  Vender(cantidad){
-    this.cantidad=this.cantidad - cantidad;
-    if(this.cantidad==0){
-      this.stock=false;
-    }
-  }
-
-  //PRODUCTOS FALLADOS QUE DEVOLVIERON, EN CASO DE SER MAYOR AL 30% 
-  Devolucion(fallados){
-    this.fallados=this.fallados+fallados;
-    if(this.fallados<this.cantidad*0,30){
-      alert('NO COMPRAR PRODUCTO')
-    }
+    this.tipo=tipo;
+    this.precio=parseFloat(precio);
   }
 }
+let arrayBaterias=[];
 
-//INGRESO MANUAL DE PILAS
-let pilas1 =new Pilas ('aaa', 200)
-//COMPROBACION DE LAS VARIABLES
-console.log(pilas1)
-pilas1.Iva();
-pilas1.Vender(3);
-pilas1.Devolucion(7);
+arrayBaterias.push(new pilas("aaa","200"));
+arrayBaterias.push(new pilas("aa","200"));
+arrayBaterias.push(new pilas(prompt("tipo"),prompt("precio")));
 
 
-//INGRESO POR PROMPT DE PILAS
 
-for(let index =0; index<3;index++){
-  let pilas=new Pilas(
-    prompt('tipo de pila'),
-    prompt('precio de pila'),
-    );  
+//lista de baterias con DOM
+let tituloBaterias = document.createElement("ul");
+tituloBaterias.innerHTML="<ul>baterias</ul>";
+document.body.appendChild(tituloBaterias);
+
+//agregar baterias con DOM
+let listaBaterias = document.getElementById("arrayBaterias");
+
+for (const pila of arrayBaterias){
+  let li=document.createElement("li");
+  li.innerHTML=arrayBaterias
+  tituloBaterias.appendChild(li);
 }
+
